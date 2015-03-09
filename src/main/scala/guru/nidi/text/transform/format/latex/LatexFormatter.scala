1b2689229d244783b184e648dc983dc5711d6c18
Stefan Niederhauser <ghuder5@gmx.ch>
2015-03-09 14:07:01 +0100

    add license

--- src/main/scala/guru/nidi/text/transform/format/latex/LatexFormatter.scala
+++ src/main/scala/guru/nidi/text/transform/format/latex/LatexFormatter.scala
@@ -1,10 +1,24 @@
+/**
+ * Copyright (C) 2013 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package guru.nidi.text.transform.format.latex
 
-import guru.nidi.text.transform._
 import guru.nidi.text.transform.Attribute._
-import guru.nidi.text.transform.Name._
 import guru.nidi.text.transform.AttributeValue._
-import scala.Some
+import guru.nidi.text.transform.Name._
+import guru.nidi.text.transform._
 
 class LatexFormatter(val context: TransformContext) extends Formatter {
   def format(segment: Segment) = {
--- src/main/scala/guru/nidi/text/transform/parse/html/HtmlParser.scala
+++ src/main/scala/guru/nidi/text/transform/parse/html/HtmlParser.scala
@@ -1,20 +1,36 @@
+/**
+ * Copyright (C) 2013 Stefan Niederhauser (nidin@gmx.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package guru.nidi.text.transform.parse.html
 
-import guru.nidi.text.transform.{AttributeValue, Segment, TransformContext}
-import guru.nidi.text.transform.Name._
+import java.io.StringReader
+import java.util.regex.Pattern
+
 import guru.nidi.text.transform.Attribute._
 import guru.nidi.text.transform.AttributeValue._
+import guru.nidi.text.transform.Name._
 import guru.nidi.text.transform.Segment._
-
-import collection.mutable.ListBuffer
-import java.util.regex.Pattern
-import org.xml.sax.InputSource
-import java.io.StringReader
-import xml.parsing.{NoBindingFactoryAdapter, FactoryAdapter}
-import xml.factory.XMLLoader
 import guru.nidi.text.transform.parse.AbstractParser
+import guru.nidi.text.transform.{AttributeValue, Segment, TransformContext}
 import org.apache.commons.lang3.text.translate._
-import xml.{NodeSeq, Elem, Node, Text}
+import org.xml.sax.InputSource
+
+import scala.collection.mutable.ListBuffer
+import scala.xml.factory.XMLLoader
+import scala.xml.parsing.{FactoryAdapter, NoBindingFactoryAdapter}
+import scala.xml.{Elem, Node, NodeSeq, Text}
 
 /**
  *
